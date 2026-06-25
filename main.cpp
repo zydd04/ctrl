a0432809f624de5c7724ca88ae7a3664b210b45f
Zydd04 <ziyad.elfettahi@mail.mcgill.ca>
2026-06-25 14:33:46 +0100

    Added API key Generation Function

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <random>
+
+#include <iostream>
+
+//function to generate random API key
+std::string genAPIK(size_t length = 28) {
+    const std::string chars =
+        "12334567890" "QWERTYIOPASDFGHJKLZXCVBNM";
+    std::random_device ran;
+    std::uniform_int_distribution<size_t> dist(0, chars.size() - 1);
+    std::string key;
+    key.reserve(length);
+    for (size_t i = 0; i < length; ++i) {
+        key += chars[dist(ran)];
+    }
+    return key;
+}
+
+int main() {
+	std::cout << "Welcome to the CLI CTRLai App!" << "\n" ;
+	std::cout << "Your Generated API Key: " << genAPIK() << "\n";
+	return 0;
+}
+
