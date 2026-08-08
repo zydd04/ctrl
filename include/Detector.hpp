7515be4df91ea6f55e2e1bfd0b1956d273e31352
Zydd04 <ziyad.elfettahi@mail.mcgill.ca>
2026-08-09 00:20:32 +0100

    Adde main window header

--- include/Detector.hpp
+++ include/Detector.hpp
@@ -5,23 +5,24 @@
 #include <chrono>
 #include <algorithm>
 #include <opencv2/opencv.hpp>
-#include "Capture.h"
-#include "InputController.h"
+#include "Capture.hpp"
+#include "InputController.hpp"
 
 //Detects actual hand gestures from the Webcam capture and translates them into key press values
 
 class Detector {
 public:
-	explicit Detector(std::unordered_map<std::string,WORD> gestureToKey);
+	explicit Detector(std::unordered_map<std::vector<std:string>,WORD> gestureToKey, int threshhold);
 
 	//Detects hand gesture type and returns key
-	WORD Detect(cv::Mat& frame) const {return keyPress};
+	WORD Detect(cv::Mat& frame) const {return keyPress;}
 
 	//Outputs Fps counter for the Webcam capture
 	void DrawFps(cv::Mat& frame); 
 private:
-	std::unordred_map<std::string, WORD> gestureToKey_;
+	std::unordred_map<std::vector<std::string>, WORD> gestureToKey_;
 	WORD keyPress_;
+	int threshhold;
 
 };
 
---
+++ include/MainWindow.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+#include "Capture.hpp"
+#include "Detector.hpp"
+#include "InputController.hpp"
+
+//Creates the main window of the app
+class MainWindow {
+public:
+	explicit MainWindow(int wWidth = 800, int wHeight = 400);
+	
+	//Returns hwnd handle
+	HWND getH() const {return hwnd_;}
+
+	//Handles Errors
+	void ShowError(std::wstring& text, std::wsting& title) const;
+	//Shows information
+	void ShowInfo(std::wstring& text, std::wstring& title) const;
+
+	//Starts the Webcam for testing of functionality of the app
+	void StartStreamTest() const;
+	
+	//Runs the App
+	void Run() const;
+
+	//Stops the App
+	void Stop() const;
+private:
+	LRESULT CALLBACK WindowProcess(HWND, UINT, WPARAM, LPARAM);
+	LRESULT HandleMessage(UINT msg, WPARAM wp, LPARAM lp);
+
+	int wWidth_;
+	int wHeight_;
+
+	void AddMenu();
+	void AddControl();
+
+	HWND hwnd_ = nullptr;
+	HMENU menu_ = nullptr;
+	
+	enum MenuId = {Connectid = 1, Helpid = 2, Aboutid = 4, Stopid = 5, Runid = 6, Testid = 7}
+};	
+
+
+
